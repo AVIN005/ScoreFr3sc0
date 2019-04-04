5120cf2e4fdb299fae3282191ba750412221c75c
Balasa Avinash <balasa.avinash@gmail.com>
2019-04-05 01:59:20 +0530

    array to arraylist

--- lab/src/main/java/njsnarya.java
+++ lab/src/main/java/njsnarya.java
@@ -1,9 +1,14 @@
 import com.google.gson.Gson;
+import com.sun.deploy.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class njsnarya {
 
     public static void main(String[] args) {
-        String json = "[\n" +
+       /* String json = "[\n" +
                 "        {\n" +
                 "            \"number\" : \"3\",\n" +
                 "                \"title\" : \"hello_world\"\n" +
@@ -14,11 +19,58 @@ public class njsnarya {
                 "        }\n" +
                 "]";
         Gson gson = new Gson();
-        Wrapper[] temp = gson.fromJson(json, Wrapper[].class);
-        System.out.println(temp[1].number);
+        Wrappers[] temp = gson.fromJson(json, Wrappers[].class);
+        System.out.println(temp[1].number);*/
+        Wrappers[] array = {new Wrappers(1), new Wrappers(2), new Wrappers(3)};
+        System.out.println(array[1].number + array[2].number + "" + array[0].number);
+        /*ArrayList<Wrappers> arrayList = new ArrayList<Wrappers>(Arrays.asList(array));
+        final int yo = arrayList.get(1).number;
+        System.out.println(yo);*/
+
+        System.out.println(arrayToList(array));
+        //System.out.println(arrayToList(array).toString());
     }
-    class Wrapper{
+
+        static List arrayToList(final Object[] array) {
+            final List l = new ArrayList(array.length);
+
+            for (int i = 0; i < array.length; i++) {
+                l.add(array[i]);
+            }
+            return l;
+        }
+      /* static <T> List<T> arrayToList(final T[] array) {
+           final List<T> l = new ArrayList<T>(array.length);
+
+           for (final T s : array) {
+               l.add(s);
+           }
+           return (l);
+       }*/
+
+
+
+    static class Wrappers{
         int number;
         String title;
+
+        public Wrappers(int i) {
+            this.number = i;
+        }
+
+        public Wrappers() {
+
+        }
+
+        @Override
+        public String toString() {
+            return "Wrappers{" +
+                    "number=" + number +
+                    ", title='" + title + '\'' +
+                    '}';
+        }
+        /*public String toString() {
+            return Integer.toString(number);
+        }*/
     }
 }
